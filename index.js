f11cf40fb36f8bc81bb473ad01a442cf319c1f62
ruby <916809498@qq.com>
2019-05-27 19:43:45 +0800

    fix env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 let exec = require('child_process').exec
+require('dotenv').config()
 
 const app = express()
 
@@ -17,7 +18,7 @@ app.post('/blog', (req, res) => {
   ].join('&&')
   exec(command + ' > /var/log/webhook-blog.log 2>&1', (error, stdout, stderr) => {
   })
-  res.send('Hello World!')
+  res.send(command)
 })
 
 app.listen(8600, () => console.log('Example app listening on port 3000!'))
