44e7da81c0f392cced0b56247c1b5f9f2d9c5225
ruby <916809498@qq.com>
2019-11-05 17:56:08 +0800

    webhook

---
+++ frontend.js
@@ -0,0 +1,21 @@
+const express = require('express')
+let exec = require('child_process').exec
+require('dotenv').config()
+
+const app = express()
+
+app.post('/frontend', (req, res) => {
+  let codeDir = '/Users/ruby/Code/eb-frontend'
+  let command = [
+    `cd ${codeDir}`,
+    `git pull`,
+    `./deploy.sh`,
+    `php /Users/ruby/Code/eb-backend/artisan mail --extra=frontend`
+  ].join('&&')
+  exec(command + ' > /var/log/webhook-blog.log 2>&1', (error, stdout, stderr) => {
+  })
+  res.send(command)
+})
+
+const port = 14000
+app.listen(port, () => console.log(`app listening on port ${port}!`))
